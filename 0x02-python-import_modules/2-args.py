a2b8dc059b84a457e300b5423a4e2611d4f15497
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-04 00:05:46 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     import sys
     count = len(sys.argv) - 1
     if count == 0:
-        print("{} arguments".format(0))
+        print("{} arguments.".format(0))
     elif count == 1:
         print("{} argument".format(1))
     else:
