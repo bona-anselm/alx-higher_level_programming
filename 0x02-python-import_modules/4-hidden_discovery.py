ea538c62fd3a5be73aba1031ecf232b78378ea70
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-04 01:17:17 +0000

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == '__main__':
     import hidden_4
     list_dir = dir(hidden_4)
     for name in list_dir:
-        if list_dir[:2] != '__':
+        if name[:2] != '__':
             print("{}".format(name))
 
