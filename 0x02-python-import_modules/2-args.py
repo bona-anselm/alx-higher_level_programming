aca3ca50aab1054f6b6571b0c59df1219db1089d
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-04 00:28:57 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     elif len(sys.argv) == 2:
         print("{} argument:".format(1))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1)i)
+        print("{} arguments:".format(len(sys.argv) - 1))
         if len(sys.argv) > 1:
             for i in range(1, len(sys.argv)):
                 print("{}: {}".format(i, sys.argv[i]))
