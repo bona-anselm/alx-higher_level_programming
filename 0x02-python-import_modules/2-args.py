5a7b18f484d2d12daa75163d1aad488849dcbfe3
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-04 00:01:19 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    count = len(sys.argv)
-    if count == 1:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("{} arguments".format(0))
-    elif count == 2:
+    elif count == 1:
         print("{} argument".format(1))
     else:
-        print("{} arguments".format(len(sys.argv) - 1))
+        print("{} arguments".format(count))
         if count > 1:
             for i in range(1, len(sys.argv)):
                 print("{}: {}".format(i, sys.argv[i]))
