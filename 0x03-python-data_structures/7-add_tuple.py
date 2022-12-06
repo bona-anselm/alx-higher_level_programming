7e6fb906babdfc2ae31219dbaa737f63a13585de
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-06 11:13:39 +0000

    Task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,7 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     if len_tuple_a == 0:
         a1 = 0
-        b1 = 0
+        a2 = 0
     elif len_tuple_a == 1:
         a1 = tuple_a[0]
         a2 = 0
