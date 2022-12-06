637b64ec2f6bae44e2ac3e12677ccd4da803e565
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-06 11:47:31 +0000

    Task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0]
+
+    if length == 0:
+        new_tuple = (length, None)
+    else:
+        new_tuple = (length, first_char)
+    
+    return (new_tuple)
