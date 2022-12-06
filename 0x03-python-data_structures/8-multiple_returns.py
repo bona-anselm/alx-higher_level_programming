f202569f474c30ec547ea19165826e4aa020215d
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-06 23:02:57 +0000

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len_sentence = len(sentence)
+    length = 0
+    first_char = None
+    for i in sentence:
+        if length == 0:
+            first_char = i
+        length += 1
+    return_tuple = (length, first_char)
+    return (return_tuple)
+'''    len_sentence = len(sentence)
     first_char = sentence[0]
-
-    if not sentence:
-        sentence = None
 
     if len_sentence == 0:
         new_tuple = (len_sentence, None)
     else:
         new_tuple = (len_sentence, first_char)
 
-    return new_tuple
+    return new_tuple'''
