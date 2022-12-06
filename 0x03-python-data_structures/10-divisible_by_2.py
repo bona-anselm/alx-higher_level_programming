c81ee81a2868a5fcf1c9d6c3a1f8a17776976374
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-06 13:09:16 +0000

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if not my_list:
         return my_list
 
-    new_list = my_list[:]
+    new_list = list(my_list)
     for i in new_list:
         if i % 2 == 0:
             new_list[i] = True
