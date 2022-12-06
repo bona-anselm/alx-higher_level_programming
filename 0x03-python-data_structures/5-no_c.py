cd313aa3bc2db8da3015f81119e29a0761723907
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-06 12:19:40 +0000

    Task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    list_my_string  = list(my_string)
+    idx_count = 0
+
+    for i in list_my_string:
+        if i == 'c' or i == 'C':
+            list_my_string[idx_count] = ""
+        idx_count += 1
+    return "".join(list_my_string)
