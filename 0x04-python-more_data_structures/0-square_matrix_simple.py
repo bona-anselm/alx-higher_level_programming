2a9a159b0b9d85206e44d290a33e784592200c7d
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-08 15:28:27 +0000

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-
-    for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-
-    return (new_matrix)
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
