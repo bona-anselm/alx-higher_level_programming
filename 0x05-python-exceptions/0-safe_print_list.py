f21010dd4b1804880b91ba1480b84c6bac0ff359
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-19 20:44:47 +0000

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    count = 0
+
+    for element in my_list:
+        if count < x:
+            try:
+                print(f"{element}", end='')
+                count += 1
+            except IndexError:
+                break
+    print()
+    return count
