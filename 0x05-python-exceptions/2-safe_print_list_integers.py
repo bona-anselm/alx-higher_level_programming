65b3ed997bd9db7555ba97efec60638524da68aa
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-19 21:15:13 +0000

    Task 0

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for index in range(0, x):
             try:
-                print("{}".format(my_list[index]), end='')
+                print("{:d}".format(my_list[index]), end='')
                 count += 1
             except (TypeError, ValueError):
                 continue
