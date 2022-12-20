850cbae778c271a18bf955ba36aaa17d52c1c76a
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-20 01:04:50 +0000

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except (ValueError, TypeError):
             print("wrong type")
         except ZeroDivisionError:
-            print("division by zero")
+            print("division by 0")
         except IndexError:
             print("out of range")
         finally:
