5132f778c7831628aff8b8f6c96f2a3260f80b6a
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-22 08:51:35 +0000

    Tassk 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class Square:
+
+    """
+    This is an empty class that defines a square
+
+
+    Args:
+        None - its an empty class
+
+
+    Attributes:
+        It has no attribute as it's empty
+    """
