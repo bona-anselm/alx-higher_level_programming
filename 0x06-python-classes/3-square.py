b1d736ea4a10cec0ac67cd6c4b7b98a55d2d379e
Bonaventure Anselm <bona.anselm@gmail.com>
2022-12-23 22:56:16 +0000

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""No module imported"""
+
+
+class Square:
+    """Defines a class: Square with a private
+       attribute: size and checks Exceptions
+    """
+
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return pow(self.__size, 2)
