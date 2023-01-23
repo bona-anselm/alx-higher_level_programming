8a3dbf1dd5a52b5333ac41b4fd71cda7ab62b6a6
Bonaventure Anselm <bona.anselm@gmail.com>
2023-01-23 07:44:26 +0000

    Updated

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -32,9 +32,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be an integer")
+            raise ValueError("height must be an integer")
         else:
             self.__height = value
 
