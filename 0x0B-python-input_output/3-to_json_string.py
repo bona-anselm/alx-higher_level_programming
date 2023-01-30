fd7eaf366ee53c6823023112d3e3868381ad7583
Bonaventure Anselm <bona.anselm@gmail.com>
2023-01-30 04:16:00 +0000

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-    module that creates a function that  returns the JSON representation        of an object (string)
+    creates a function that
+    returns the JSON representation
+    of an object (string)
 """
 import json
 
