1e739c82b74b8b381a7aae8ee221c5d8b74b0250
Bonaventure Anselm <bona.anselm@gmail.com>
2023-01-30 06:37:45 +0000

    Task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    creates a function that
+    returns an object (python data structure) represented
+    of an object (string)
+"""
+import json
+
+
+def from_json_string(my_str):
+    """  returns an object (Python data structure) represented 
+            by a JSON string:
+    """
+    return json.loads(my_str)
