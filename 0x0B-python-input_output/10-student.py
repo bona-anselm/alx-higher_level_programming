6bf021088a9c46c6f055d48e755f7e126d6f72d5
Bonaventure Anselm <bona.anselm@gmail.com>
2023-01-30 09:24:38 +0000

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Defines a class Student"""
+
+
+class Student:
+    """Defines Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
