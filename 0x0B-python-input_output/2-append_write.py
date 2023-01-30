b04904aa2ddd3d3d779c2102039d422d4ebeecfd
Bonaventure Anselm <bona.anselm@gmail.com>
2023-01-30 03:56:45 +0000

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """ module to create a function: write_file """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ writes a string to a text file (UTF8) and returns
         the number of characters written
     """
