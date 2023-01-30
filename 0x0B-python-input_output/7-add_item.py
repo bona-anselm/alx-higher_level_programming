d8fb58e6ddec6f463c3f5d353ade52eeb1468f48
Bonaventure Anselm <bona.anselm@gmail.com>
2023-01-30 08:41:10 +0000

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,10 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args_lists = load_from_json_file("add_item.json")
+try:
+    args_lists = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    args_lists = []
 
 for args in sys.argv[1:]:
     args_lists.append(args)
