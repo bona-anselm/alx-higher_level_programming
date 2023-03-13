6c5ada72b5034e9df1f764f43bd761f623a78dc4
Bonaventure Anslem <bona.anselm@gmail.com>
2023-03-13 23:21:01 +0000

    Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const cString = 'C is fun';
+const pythonString = 'Python is cool';
+const javascriptString = 'JavaScript is amazing';
+
+console.log(cString);
+console.log(pythonString);
+console.log(javascriptString);
