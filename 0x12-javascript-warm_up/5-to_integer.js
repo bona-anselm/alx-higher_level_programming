1ce1cd322077ec0e5c5800bba14814de0e0848c8
Bonaventure Anslem <bona.anselm@gmail.com>
2023-03-14 13:56:35 +0000

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const argPassed = Number(parseInt(process.argv[2]));
+const argPassed = Math.floor(Number(parseInt(process.argv[2])));
 // const argConverted = parseInt(argPassed[2]);
 
-if (isNaN(Math.floor(argPassed))) {
+if (isNaN(argPassed)) {
   console.log('Not a Number');
 } else {
   console.log(`My number: ${argPassed}`);
