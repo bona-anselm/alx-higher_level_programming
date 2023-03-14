f30739365f61e0108d264d4d5563ecd20f7e8175
Bonaventure Anslem <bona.anselm@gmail.com>
2023-03-14 15:22:54 +0000

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,10 @@
 const squareSize = parseInt(process.argv[2]);
 
 if (isNaN(squareSize)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < squareSize; i++) {
-    let square = "";
+    let square = '';
     for (let j = 0; j < squareSize; j++) {
       square += 'X';
     }
