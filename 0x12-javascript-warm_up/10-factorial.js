fca4cc2f09e74addbd5ca93656e2636552bf0c36
Bonaventure Anslem <bona.anselm@gmail.com>
2023-03-14 17:07:40 +0000

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const numArg = parseInt(process.argv[2]);
+
+
+function factorial (numArg) {
+  if (isNaN(numArg) || numArg === 0) {
+    return 1;
+  } else {
+    return numArg * factorial(numArg - 1);
+  }
+}
+console.log(factorial(numArg));
+
