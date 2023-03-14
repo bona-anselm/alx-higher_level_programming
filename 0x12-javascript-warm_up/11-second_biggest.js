a8b49da904dd9504b095f1b7550332a235ce224d
Bonaventure Anslem <bona.anselm@gmail.com>
2023-03-14 22:11:49 +0000

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,18 +6,18 @@ if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
   const numArgs = args.map(num => parseInt(num));
-  
+
   let maxNumber = -Infinity;
   let secondMaxNumber = -Infinity;
-   
+
   for (let i = 0; i < numArgs.length; i++) {
     if (numArgs[i] > maxNumber) {
       secondMaxNumber = maxNumber;
       maxNumber = numArgs[i];
-  } else if (numArgs[i] > secondMaxNumber && numArgs[i] !== maxNumber) {
-    secondMaxNumber = numArgs[i];
+    } else if (numArgs[i] > secondMaxNumber && numArgs[i] !== maxNumber) {
+      secondMaxNumber = numArgs[i];
+    }
   }
-}
   if (secondMaxNumber === -Infinity) {
     console.log(0);
   } else {
