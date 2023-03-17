25649bd3bd426bfbfcc5f8f016b43122ca0fccc1
Bonaventure Anslem <bona.anselm@gmail.com>
2023-03-17 23:07:34 +0000

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,13 +3,12 @@
 const formerSquare = require('./5-square');
 
 class Square extends formerSquare {
-
   charPrint (c) {
-  if (c === undefined) {
-    this.print();
-  } else {
+    if (c === undefined) {
+      this.print();
+    } else {
       const squareSymbol = 'C'.repeat(this.width);
-    
+
       for (let i = 0; i < this.height; i++) {
         console.log(squareSymbol);
       }
