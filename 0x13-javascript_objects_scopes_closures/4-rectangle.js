e516a051dcec65fa15e6e43b05d543013f72ab14
Bonaventure Anslem <bona.anselm@gmail.com>
2023-03-17 19:38:45 +0000

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let squareSymbol = '';
+      for (let j = 0; j < this.width; j++) {
+        squareSymbol += 'X';
+      }
+      console.log(squareSymbol);
+    }
+  }
+
+  double() {
+    this.height *= 2;
+    this.width *= 2;
+  }
+
+
+  rotate() {
+    const brief = this.height;
+    this.height = this.width;
+    this.width = brief;
+  }
+};
