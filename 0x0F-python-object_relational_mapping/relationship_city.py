a63247cb9f2c4e5376744cb01f1a066ec9117e06
Bonaventure Anselm <bona.anselm@gmail.com>
2023-03-23 08:57:18 +0100

    Task 100

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class City"""
+
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ Represents a city column for a MySQL table
+    __tablename__ (str): The name of the MySQL table to store Cities.
+    id (sqlalchemy.Integer): The city's id.
+    name (sqlalchemy.String): The city's name.
+    state_id(sqlalchemy.Integer): Foreign key to states.id
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
