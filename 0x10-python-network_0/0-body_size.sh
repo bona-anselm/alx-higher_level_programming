3cfdf5679fe818aec63de7f6417ef9de57b9ac2a
Bonaventure Anselm <bona.anselm@gmail.com>
2023-03-30 19:47:18 +0100

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
-# check if URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 URL"
-  exit 1
-fi
-
-# send request to URL and save response to a temporary file
-curl -s "$1" -o /tmp/response.txt
-
-# get size of response body in bytes
-size=$(wc -c < /tmp/response.txt)
-
-# display size of response body
-echo $size
-
-# remove temporary file
-rm /tmp/response.txt
+#!/bin/bash
+# Script that shows the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
