1d11608d1bb49064b11d462f9366b5bb93623893
Bonaventure Anselm <bona.anselm@gmail.com>
2023-04-03 07:27:12 +0100

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,14 @@
     A script that fetches https://alx-intranet.hbtn.io/status using the
     urllib package
 """
-
 from urllib import request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
 
-with request.urlopen(url) as r:
-    data = r.read()
+    with request.urlopen(url) as r:
+        data = r.read()
 
-print("Body response:\n\t - type: {}\n\t - content: {}\n\t - utf8 content: {}".format(type(data), data, data.decode('utf-8')))
+    print("Body response:\n\t - type: {}\n\t - content: {}\n\t"
+          "- utf8 content: {}"
+          .format(type(data), data, data.decode('utf-8')))
