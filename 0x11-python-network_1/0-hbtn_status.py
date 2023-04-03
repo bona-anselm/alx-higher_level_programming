e74a89dabb31dcab0a7256338106147b76b08bf0
Bonaventure Anselm <bona.anselm@gmail.com>
2023-04-03 07:17:02 +0100

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,4 @@ url = 'https://alx-intranet.hbtn.io/status'
 with request.urlopen(url) as r:
     data = r.read()
 
-print(f"Body response:")
-print(f"\t - type: {type(data)}")
-print(f"\t - content: {data}")
-print(f"\t - utf8 content: {data.decode('utf-8')}")
+print("Body response:\n\t - type: {}\n\t - content: {}\n\t - utf8 content: {}".format(type(data), data, data.decode('utf-8')))
