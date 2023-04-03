bd4cf36b4cb42b4543214bd781d7b8620b316f21
Bonaventure Anselm <bona.anselm@gmail.com>
2023-04-03 06:49:16 +0100

    README and Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    A script that fetches https://alx-intranet.hbtn.io/status using the
+    urllib package
+"""
+
+from urllib import request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with request.urlopen(url) as r:
+    data = r.read()
+
+print(f"Body response:\n\t - type: {type(data)}\n\t"
+      f" - content: {data}\n\t "
+      f"- utf8 content: {data.decode('utf-8')}")
