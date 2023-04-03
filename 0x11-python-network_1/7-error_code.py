b8724f7e37a5a73076db480a530cb878c894ccf1
Bonaventure Anselm <bona.anselm@gmail.com>
2023-04-03 16:22:16 +0100

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL, sends a request to the URL
+    and displays the body of the response (decoded in utf-8). It
+    also manages urllib.error.HTTPError exceptions and print: Error
+"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    request_resp = requests.get(url)
+    
+    if request_resp.status_code < 400:
+        print(request_resp.text)
+    else:
+        print(f"Error code: {request_resp.status_code}")
